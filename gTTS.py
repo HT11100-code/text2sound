4fe254d5c37ceffc8f535c76f4145a2a0238be9e
haruto <takahashiharuto@TakahashinoMacBook-Pro.local>
2025-05-21 13:15:36 +0900

    add gTTS

--- gTTS.py
+++ gTTS.py
@@ -1,8 +1,9 @@
+#pip install gttsをする必要あり（requirements.txtに追加してない）
 from gtts import gTTS
 
 def read_text(text):
     tts = gTTS(text=text, lang='en')
-    tts.save("output.mp3")
+    tts.save("output_file.mp3")
 
 if __name__ == "__main__":
     text = input("Enter the text to read: ")

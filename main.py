0032fa84bbf50a9d2637ebae1c68eda280b85ead
haruto <takahashiharuto@TakahashinoMacBook-Pro.local>
2025-05-21 10:44:32 +0900

    Update setting

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import pyttsx3
 
 def read_text(text):
     engine = pyttsx3.init()
+    
+    rate = engine.getProperty('rate')
+    #engine.setProperty('rate', rate - 50)
+    #読み上げ速度の変更
+
+    #voices = engine.getProperty('voices') 現在のボイスの取得
+    #engine.setProperty('voice', voices[0].id)　ボイスの変更
+    
+
     engine.say(text)
     engine.runAndWait()
+    
 
 if __name__ == "__main__":
     text = input("Enter the text to read: ")
